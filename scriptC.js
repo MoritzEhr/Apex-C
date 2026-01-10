544f3681bcd6aad51980319db2d3ae6add6d6ca8
moritz <moritz.ehrhardt123@gmail.com>
2026-01-10 21:54:37 +0100

    Add item_list to GA4 purchase tracking

--- scriptC.js
+++ scriptC.js
@@ -48,7 +48,7 @@ function trackPurchaseCompleted() {
       gtag('event', 'purchase_completed', {
         transaction_id: transactionId,
         proid_token: proid,
-        items: cart.items,
+        item_list: cart.items,
         sessionid_token: sessionid,
         studyid_token: studyid,
         tip_percentage: tipPercentage,
